719bc5a139bf807ac79c191aebe8cfbe61a183e3
Mila <milaprivalova1@gmail.com>
2022-10-17 16:42:43 +0300

    task2

--- dz6.py
+++ dz6.py
@@ -3,15 +3,31 @@
 # делитель (НОД) двух чисел. Вычислите и напечатайте наибольший общий делитель
 #  для списка натуральных чисел. Ввод чисел продолжается до ввода пустой строки.
 
-from math import gcd
-from functools import reduce
+# from math import gcd
+# from functools import reduce
 
-lst = []
+# lst = []
+# while True:
+#    num = input('Введите число: ')
+#    if num == '':
+#        break
+#    lst.append(int(num))
+# print('Общий делитель: ' + str(reduce(gcd, lst)))
 
-while True:
-    num = input('Введите число: ')
-    if num == '':
-        break
-    lst.append(int(num))
+# 2. Орел и решка
 
-print('Общий делитель: ' + str(reduce(gcd, lst)))
+text = 'ООООРРРРРРРРРРРООООО'
+char_to_find = 'Р'
+max_count = 0
+tmp = 0
+for b in text:
+    if char_to_find == b:
+        tmp += 1
+    else:
+        if tmp > max_count:
+            max_count = tmp
+        tmp = 0
+if tmp > max_count:
+    max_count = tmp
+print(f'Последовательность: {text}')
+print(f'Максимальная последовательность решки: {max_count}')
